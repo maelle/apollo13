cbbabadd31068349b28861c64943a0aa7addb84e
Maëlle Salmon <maelle.salmon@yahoo.se>
2021-05-27 14:30:14 +0200

    update

--- R/packages.R
+++ R/packages.R
@@ -7,7 +7,7 @@
 #'
 #' @examples
 #' get_packages("ropensci")
-get_packages <- function(universe = "ropensci") {
+get_packages <- function(universe) {
   url <- sprintf("https://%s.r-universe.dev/packages", universe)
   resp <- httr::GET(url)
   httr::stop_for_status(resp)
